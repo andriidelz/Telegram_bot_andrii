3b409ecab3e5ac22e153b8e0f6dfbe3810c8592a
andriidelz <zaliubovskiy25@gmail.com>
2024-02-28 22:41:55 +0200

    rm training 1

--- training/maintraining.py
+++ training/maintraining.py
@@ -1,25 +1,41 @@
-import asyncio
 import logging
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart, Command
+from aiogram.utils.markdown import hbold
 
 from token_api import TOKEN_API
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
 
-token = "6900592213:AAHJTfRX2vYdnmYivJh3vAXWloPqEaBKsX4"
+
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
-async def cdm_start(msg: types.Message) -> None:
-    await msg.answer(
-        text = 'Hello World!'
-    )
+async def cmd_start(msg: types.Message) -> None:
+    reply_text = f'Hello, {hbold(msg.from_user.full_name)}. It has commands /help, '
+    await msg.answer(text=reply_text)
+
+
+@dp.message(Command(commands=['help']))
+async def help(msg: types.Message) -> None:
+    await msg.answer(text=f'This bot cannot do everying and nothing')
+
 
 async def main() -> None:
-    bot = Bot(TOKEN_API)
-    
+    bot = Bot(token=TOKEN_API,
+              parse_mode='HTML'
+              )   
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
+#     logging.basicConfig(
+#     format='%(levelname)s: %(message)s',
+#     level=logging.INFO,
+#     filename='app.log',
+#     filemode='w'
+# )
+    
+if __name__ == '__main__':
     logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s', level=logging.INFO)
-    asyncio.run(main()) 
+    asyncio.run(main())
